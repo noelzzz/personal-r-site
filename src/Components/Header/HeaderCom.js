74ab48fa8593fb59b2b26a01aba57babe42fea72
Noel <ndalfaro333@gmail.com>
2022-07-15 01:47:25 -0500

    changed navitem list hover behaviour

--- src/Components/Header/HeaderCom.js
+++ src/Components/Header/HeaderCom.js
@@ -112,13 +112,21 @@ const NavItem = styled.li`
     margin: 2%;
     transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
     /* font-size: 18px; */
-
+    text-decoration: none;
+    background-image: linear-gradient(to right, #000, #000);
+    background-position: bottom left;
+    background-repeat: no-repeat;
+    background-size: 0% 2px;
+    transition: background-size .5s ease;
 
     &:hover{
-        -webkit-text-decoration: slateblue solid underline;
+        /* -webkit-text-decoration: slateblue solid underline;
         text-decoration: slateblue solid underline;
         -webkit-text-underline-position: under;
-        text-underline-position: under;
+        text-underline-position: under; */
+        background-size: 100% 2px;
+
+        
 }
     
     /* &:active{
